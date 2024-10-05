0467ae8b6d4e648f05afa2876524306288945617
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-10-06 07:32:01 +0800

    Finished writing Exercise 1 with a working code

--- lab2_exercise1.py
+++ lab2_exercise1.py
@@ -6,7 +6,20 @@
 # For example, to calculate 2^7, you multiply 2 by itself seven times.
 
 # Define power function that use recursion
-# Check if exponent is Zero
+def power(base, exponent):
+    # Check if exponent is Zero
+    if exponent == 0:
+        return 1
+    else:
+        return base * power(base, exponent - 1)
+
+
 # Ask the user for the input of values
+base = int(input("Please enter the base number: "))
+exponent = int(input("Please enter the exponent: "))
+
 # Return and print result
+power_result = power(base, exponent)
+print(f"\n{base} raised to the power of {exponent} is: {power_result}")
+
 # End
