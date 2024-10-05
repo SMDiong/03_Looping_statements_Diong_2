c1c10f825177d6701f7d519225a19405af9baa02
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-10-06 07:48:56 +0800

    Finished writing Exercise 2 with a working code

--- lab2_exercise2.py
+++ lab2_exercise2.py
@@ -5,9 +5,24 @@
 # Exercise 2: Compute the cube of integers in an array.
 # The program should ask the user for the size of the array, then for the elements of the array, and it should display the cube of each element.
 
+# Word Colors
+red = "\033[0;31m"
+yellow = "\033[1;33m"
+green = "\033[0;32m"
+
 # Ask the user for the size of the array
+array_size = int(input(green + "Please enter the size of the array: "))
+
 # Ask the user to input the elements of the array, separated by spaces
+array_elements = list(map(int, input("Please enter the elements of the array separated by spaces: ").split()))
+
 # Convert the input string of numbers into an array of integers
-# Loops each element in the array
-# Print result
+if len(array_elements) != array_size:
+    print(red + f"\nError! You need to enter exactly {array_size} array elements!")
+else:
+    cube_elements = [str(element ** 3) for element in array_elements]
+
+    # Print result separated by commas
+    print(f"\nThe cube of each elements are: {yellow + ', '.join(cube_elements)}")
+
 # End
