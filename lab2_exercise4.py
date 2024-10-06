b5eb2023d140a091d36ec9c87e39f781c0969903
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-10-06 08:24:07 +0800

    Finished writing Exercise 4 with a working code

--- lab2_exercise4.py
+++ lab2_exercise4.py
@@ -4,7 +4,23 @@
 
 # Exercise 4: Generate an inverted right triangle of asterisks * based on the user's input of height n.
 
+# Define inverted triangle function
+def inverted_triangle(n):
+    # Check if the triangle height is less than 2
+    if n < 2:
+        print("Triangle height must be at least 2 to form an inverted triangle!")
+        return
+
+    # Loop each row
+    for i in range(n, 0, -1):
+        print('*' * i)
+
+
 # Ask the user to input the height n of the triangle
-# Loop each row
+inverted_triangle_height = int(input("Please enter the height of an inverted right triangle: "))
+
 # Print result
+print('\n')
+inverted_triangle(inverted_triangle_height)
+
 # End
